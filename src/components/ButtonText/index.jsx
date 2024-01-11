8e536e8835131b95341e0d27f01257ab95b8d613
Guilherme De Jesus <gui-rock02@hotmail.com>
2024-01-11 09:31:42 -0300

    Added home page and some components

--- src/components/ButtonText/index.jsx
+++ src/components/ButtonText/index.jsx
@@ -1,10 +1,11 @@
 import {Container} from './styles'
 
-export function ButtonText ({title, ...rest}){
+export function ButtonText ({title, isActive = false, ...rest}){
 
     return(
         <Container 
         type="button"
+        $isactive={isActive.toString()}
         {...rest}
         >
             {title}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,49 @@
+import {FiPlus, FiSearch} from 'react-icons/fi'
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
+
+import { Header} from '../../components/Header'
+import { Input} from '../../components/Input'
+import { Note } from '../../components/Note'
+
+import { Section} from '../../components/Section'
+import { ButtonText} from '../../components/ButtonText'
+
+export function Home(){
+    return(
+        <Container>
+            <Brand>
+             <h1>Rocketnotes</h1>
+            </Brand>
+            <Header/>
+
+            <Menu>
+                <li><ButtonText title="Todos"/></li>
+                <li><ButtonText title="React"/></li>
+                <li><ButtonText title="Node"/></li>
+            </Menu>
+
+            <Search>
+                <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
+            </Search>
+
+            <Content>
+                <Section title="Minhas notas">
+                    <Note data=
+                    {{
+                        title: 'React', 
+                        tags: [
+                            {id: '1', name: 'React'},
+                            {id: '2', name: 'Rocketseat'}
+                        ]
+                    }}/>
+                </Section>
+            </Content>
+
+            <NewNote>
+                <FiPlus/>
+                Criar nota
+            </NewNote>
+
+        </Container>
+    );
+}
