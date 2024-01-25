9952b96649d89c2bf2be85bf6a3f73932c4960b8
Guilherme De Jesus <gui-rock02@hotmail.com>
2024-01-25 18:06:07 -0300

    fix diretório SignIn t3

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { SignIn } from "../pages/Signin";
+import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
 
 
